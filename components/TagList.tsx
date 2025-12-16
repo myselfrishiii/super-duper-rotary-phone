c814fb2e66c89589c6230bfed07f993f4688501e
myselfrishiii <74891230+myselfrishiii@users.noreply.github.com>
2025-12-16 22:51:13 +0530

    Add TagList component for rendering tags

--- components/TagList.tsx
+++ components/TagList.tsx
@@ -1,1 +1,13 @@
+import Link from "next/link";
+import React from "react";
 
+export default function TagList({ tags }: { tags?: string[] }) {
+  if (!tags || tags.length === 0) return null;
+  return (
+    <div style={{ marginTop: 8 }}>
+      {tags.map((t) => (
+        <Link key={t} href={`/tags/${encodeURIComponent(t)}`} style={{ marginRight: 8 }} className="link">#{t}</Link>
+      ))}
+    </div>
+  );
+}
